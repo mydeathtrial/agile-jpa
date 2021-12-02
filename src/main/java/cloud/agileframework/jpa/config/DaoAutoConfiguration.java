53cca3e8034950e82f9a0e03594b92bdcd728a73
Mydeathtrial <mydeathtrial@163.com>
2021-12-02 18:46:09 +0800

    fix(获取数据库类型):
    获取数据库类型更改为通过连接地址获取，避免了未配置JPA的数据库类型导致的无法获取数据库类型问题

--- src/main/java/cloud/agileframework/jpa/config/DaoAutoConfiguration.java
+++ src/main/java/cloud/agileframework/jpa/config/DaoAutoConfiguration.java
@@ -3,19 +3,18 @@ package cloud.agileframework.jpa.config;
 import cloud.agileframework.jpa.dao.Dao;
 import cloud.agileframework.jpa.dictionary.DataExtendManager;
 import cloud.agileframework.spring.util.BeanUtil;
-import com.alibaba.druid.DbType;
+import com.alibaba.druid.util.JdbcUtils;
 import org.hibernate.SessionFactory;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.persistence.EntityManager;
-import java.util.Locale;
 
 /**
  * @author 佟盟 on 2017/10/7
@@ -26,23 +25,22 @@ import java.util.Locale;
 @AutoConfigureAfter({DataSourceAutoConfiguration.class, DictionaryAutoConfiguration.class})
 public class DaoAutoConfiguration {
 
-	@Bean
-	@ConditionalOnMissingBean
-	public Dao dao(JpaProperties jpaProperties) {
-		String dbType = jpaProperties.getDatabase().name();
-		Dao dao = new Dao(DbType.of(dbType.toLowerCase(Locale.ROOT)));
-		dao.setSessionFactory(BeanUtil.getApplicationContext()
-				.getBean(EntityManager.class)
-				.getEntityManagerFactory()
-				.unwrap(SessionFactory.class));
-		return dao;
-	}
+    @Bean
+    @ConditionalOnMissingBean
+    public Dao dao(DataSourceProperties dataSourceProperties) {
+        Dao dao = new Dao(JdbcUtils.getDbTypeRaw(dataSourceProperties.getUrl(), dataSourceProperties.getDriverClassName()));
+        dao.setSessionFactory(BeanUtil.getApplicationContext()
+                .getBean(EntityManager.class)
+                .getEntityManagerFactory()
+                .unwrap(SessionFactory.class));
+        return dao;
+    }
 
-	@Bean
-	@ConditionalOnMissingBean(DataExtendManager.class)
-	public DataExtendManager defaultDictionaryManager() {
-		return o -> {
+    @Bean
+    @ConditionalOnMissingBean(DataExtendManager.class)
+    public DataExtendManager defaultDictionaryManager() {
+        return o -> {
 
-		};
-	}
+        };
+    }
 }
